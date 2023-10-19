5eda4461048885f109acf15b045da5c4ee477357
Ggustas <gustavo.ssilva346@gmail.com>
2023-10-19 18:10:38 -0300

    Exercício para aula do dia 19/10
    Meu mapa mental para fazer este exercício foi:
    
    1° Pegar a velocidade do carro utilizando input transformando o que está dentro do input em tipo float.
    
    2° Criei as condições para as diferentes velocidades que o carro está usando a estrutura condicional if e else.

--- exercicios/exe1.py
+++ exercicios/exe1.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 
 
-# Verificar a velocidade do carro
-velocidade= float(input("Velocidade: "))
+# Preciso saber a velocidade do veiculo
 
-if velocidade<=20:
-    print("Esta usando a primeira marcha")
+veloc= float(input("Velocidade: "))
+
+# Agora crio as condições para cada velocidade
+
+if veloc == 0:
+    print("Você está parado")
+else:
+    if veloc >=1 and veloc <=20:
+        print("Você está na primeira marcha")
+    else:
+        if veloc >=21 and veloc <=35:
+            print("Você está na segunda marcha")
+        else:
+            if veloc >=36 and veloc <=50:
+                print("Você está na terceira marcha")
+            else:
+                if veloc >=51 and veloc <=80:
+                    print("Você está na quarta marcha")
+                else:
+                    print("Você está na quinta marcha")
